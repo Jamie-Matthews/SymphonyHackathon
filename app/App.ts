64fce92ff6938747918071e04f853d2b0bf0aed2
jamie <jamie@FCG-EliteBG2-01.th.formicary.net>
2018-09-24 22:04:20 +0100

    start project

---
+++ app/App.ts
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+export interface IAppProps {
+    viewModel: any;
+}
+
+interface IAppState {
+
+}
+
+export default class App extends React.Component<IAppProps, IAppState> {
+
+    public render(): JSX.Element {
+
+    }
+}
