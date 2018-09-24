14efe0df231bca5980784689a92379227abed07a
jamie <jamie@FCG-EliteBG2-01.th.formicary.net>
2018-09-24 22:20:35 +0100

    more bits

--- app/App.ts
+++ app/App.ts
@@ -1,16 +1,39 @@
 import * as React from "react";
+import Loader from "react-loader-spinner";
 
 export interface IAppProps {
     viewModel: any;
 }
 
-interface IAppState {
+enum ViewState {
+    Loading,
+    Failed,
+    Loaded
+}
 
+interface IAppState {
+    viewState: ViewState;
 }
 
 export default class App extends React.Component<IAppProps, IAppState> {
 
-    public render(): JSX.Element {
+    constructor(props: IAppProps) {
+        super(props);
 
+        this.state = {
+            viewState: ViewState.Loading
+        };
+    }
+
+    public render(): JSX.Element {
+        if (this.state.viewState === ViewState.Loading) {
+            return (<Loader 
+                type="Puff"
+                color="#00BFFF"
+                height="100"	
+                width="100"
+            />);
+        }
+        return <div />;
     }
 }
